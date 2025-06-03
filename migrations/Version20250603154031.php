59b86da4ecc4e47cb8018f1c6973dfb04d7c97ba
dydy89 <108930738+dydy89@users.noreply.github.com>
2025-06-03 18:13:03 +0200

    les commits de bilel

--- migrations/Version20250603154031.php
+++ migrations/Version20250603154031.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250603152709 extends AbstractMigration
+final class Version20250603154031 extends AbstractMigration
 {
     public function getDescription(): string
     {
